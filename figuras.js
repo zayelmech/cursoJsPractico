59666f3e0f7ce70ef533a0e519453a6aec46e96d
Abdiel Carreño <56939611+zayelmech@users.noreply.github.com>
2021-07-02 07:26:34 -0500

    Calculando figuras geometricas clase 6

--- figuras.js
+++ figuras.js
@@ -7,3 +7,77 @@ ctx.moveTo(0, 0);
 ctx.lineTo(200, 100);
 
 ctx.stroke();
+
+const ladoCuadrado = 5;
+console.log("Lados del cuadrado " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = 4 * ladoCuadrado;
+
+console.log(`Perimetro = ${perimetroCuadrado} cm`);
+
+const areaCuadrado = Math.pow(ladoCuadrado, 2);
+
+console.log(`Area = ${areaCuadrado} cm^2`);
+
+
+console.group("Triángulos");
+
+// const ladoTriangulo1 = 6;
+// const ladoTriangulo2 = 6;
+// const baseTriangulo = 4;
+// console.log(
+//   "Los lados del triángulo miden: "
+//   + ladoTriangulo1
+//   + "cm, "
+//   + ladoTriangulo2
+//   + "cm, "
+//   + baseTriangulo
+//   + "cm"
+// );
+
+// const alturaTriangulo = 5.5;
+// console.log("La altura del triángulo es de: " + alturaTriangulo + "cm");
+
+function perimetroTriangulo(lado1, lado2, base) {
+    return lado1 + lado2 + base;
+}
+console.log("El perímetro del triángulo es: " + perimetroTriangulo(1, 2, 3) + "cm");
+
+function areaTriangulo(base, altura) {
+    return (base * altura) / 2;
+}
+console.log("El área del triángulo es: " + areaTriangulo(2, 3) + "cmˆ2");
+
+console.groupEnd();
+
+// Código del círculo
+console.group("Círculos");
+
+// Radio
+const radioCirculo = 4;
+console.log("El radio del círculo es: " + radioCirculo + "cm");
+
+// Diámetro
+function diametroCirculo(radio) {
+    return radio * 2;
+}
+
+// PI
+const PI = Math.PI;
+console.log("PI es: " + PI);
+
+// Circunferencia
+function perimetroCirculo(radio) {
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
+}
+
+// Área
+function areaCirculo(radio) {
+    return (radio * radio) * PI;
+}
+console.log(`Diametro: ${diametroCirculo(radioCirculo)}`);
+console.log(`Perimetro: ${perimetroCirculo(radioCirculo)}`);
+console.log(`Area: ${areaCirculo(radioCirculo)}`);
+
+console.groupEnd();
